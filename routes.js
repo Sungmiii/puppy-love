096d8bb7d418dd24645b7a9d8c5e05058333e650
Ceryssc <cs.clarke90@gmail.com>
2018-10-26 11:49:51 +1300

    get id

--- routes.js
+++ routes.js
@@ -10,4 +10,15 @@ router.get('/puppies', (req, res) =>{
   })
 })
 
+router.get('/puppies/:id', (req, res)=> {
+  fs.readFile('./data.json', 'utf8', function(err,data){
+    let id = req.params.id
+    let puppyData = JSON.parse(data)
+    let myPuppie = puppyData.puppies.find( function(n){
+        return n.id = n   
+      })
+      res.render('profile', myPuppie)
+  })
+})
+
 module.exports = router
--- server.js
+++ server.js
@@ -14,18 +14,9 @@ server.use(express.static('public'));
 
 server.set('view engine', 'hbs');
 
-<<<<<<< HEAD
-server.get('/', (req, res) => {
-    const content = {
-        title: "PUPPY LOVE | HOME"
-    }
-    res.render('index', content)
-})
-=======
 server.use(express.static('public'))
 
 server.use(express.urlencoded({extended: false}))
->>>>>>> eb04d6d061c98ee3604f630b3eb8c19c139dc074
 
 server.use('/',routes)
 module.exports = server;
