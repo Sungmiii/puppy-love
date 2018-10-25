a497148ab9c5ef5f7f6296de53e44476f27956d8
Ceryssc <cs.clarke90@gmail.com>
2018-10-26 12:06:05 +1300

    fixed routes

--- routes.js
+++ routes.js
@@ -18,15 +18,30 @@ router.get('/puppies', (req, res) => {
   })
 })
 
-router.get('/puppies/:id', (req, res)=> {
-  fs.readFile('./data.json', 'utf8', function(err,data){
-    let id = req.params.id
-    let puppyData = JSON.parse(data)
-    let myPuppie = puppyData.puppies.find( function(n){
-        return n.id = n   
-      })
-      res.render('profile', myPuppie)
-  })
-})
+router.get("/puppies/:id", (req, res) => {
+  fs.readFile("./data.json", "utf8", function(err, data) {
+    var id = Number(req.params.id);
+    var puppies = JSON.parse(data).puppies;
+    for (var i = 0; i < puppies.length; i++) {
+      if (puppies[i].id === id) {
+        var puppy = puppies[i];
+        res.render("profile", puppy);
+      }
+    }
+  });
+});
+
+// router.get('/puppies/:id', (req, res)=> {
+//   fs.readFile('./data.json', 'utf8', function(err,data){
+//     let id = req.params.id
+//     let puppyData = JSON.parse(data)
+//     let myPuppie = puppyData.puppies.find( function(n){
+//         return n.id = n   
+//       })
+//       res.render('profile', myPuppie)
+//   })
+// })
+
+
 
 module.exports = router
