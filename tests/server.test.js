35e8f9dedbb1460a1521165258e99c8de12ca370
Ceryssc <cs.clarke90@gmail.com>
2018-10-26 14:29:53 +1300

    test

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,29 +1,30 @@
 let supertest = require('supertest');
-let server = require('../server');
 let cheerio = require('cheerio');
+let server =require('../server')
 
-test("check landing page is working", (done) => {
-    supertest(server).get('/').end(function (er, res) {
-        expect(err).toBeNull()
-        let expected = 'Puppy Love'
-        let $ = cheerio.load(res.text)
-        let actual = $('h1').first().text()
-        expect(actual).toEqual(expected)
+// test("check landing page is working", (done) => {
+//     supertest(server).get('/').end(function (err, res) {
+//         expect(err).toBeNull()
+//         let expected = 'PUPPY LOVE'
+//         let $ = cheerio.load(res.text)
+//         let actual = $('a').first().text()
+//         expect(actual).toEqual(expected)
 
-        done()
+//         done()
 
-    })
-})
+//     })
+// })
 
-test("check that image is matching id", (done) => {
-    supertest(server).get('/').end(function (er, res) {
+test("check text id is correct", (done) => {
+    supertest(server).get('/').end(function (err, res) {
+        console.log(res.text)
         expect(err).toBeNull()
         let expected = 'Fido'
         let $ = cheerio.load(res.text)
-        let actual = $('.id').first().text()
+        let actual = $('h2').first().text()
         expect(actual).toEqual(expected)
 
         done()
 
     })
-})
+}) 
